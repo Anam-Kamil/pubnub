01af35301e3f8fbae46b485dba1ec3dec440bae4
Anam Kamil <anam.kamil@yasglobal.com>
2016-08-25 12:16:01 +0500

    Update user-channel-groups.js

--- jsv4/design-patterns/advanced-channel-groups/user-channel-groups.js
+++ jsv4/design-patterns/advanced-channel-groups/user-channel-groups.js
@@ -1,21 +1,29 @@
 // Add ch-user-a-present to cg-user-a-friends
 p.channelGroups.addChannels(
-        {
-                channels: ["ch-user-a-present"],
-                channelGroup: "cg-user-a-friends",
-        },
-        function (status) {
-                console.log("CG-Add: ", status);
+    {
+        channels: ["ch-user-a-present"],
+        channelGroup: "cg-user-a-friends",
+    },
+    function(status) {
+        if (status.error) {
+            console.log("operation failed w/ status: ", status);
+        } else {
+            console.log("Channel added to channel group")
+        }
     }
 );
- 
+  
 // Add ch-user-a-present to cg-user-a-status-feed
 p.channelGroups.addChannels(
-        {
-                channels: ["ch-user-a-present"],
-                channelGroup: "cg-user-a-status-feed",
+    {
+        channels: ["ch-user-a-present"],
+        channelGroup: "cg-user-a-status-feed",
     },
-        function (status) {
-                console.log("CG-Add: ", status);
+    function(status) {
+        if (status.error) {
+            console.log("operation failed w/ status: ", status);
+        } else {
+            console.log("Channel added to channel group")
+        }
     }
 );
