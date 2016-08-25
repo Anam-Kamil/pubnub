2b507510af0edc5e8545a83bf8845003786b5f9c
Anam Kamil <anam.kamil@yasglobal.com>
2016-08-25 12:20:59 +0500

    Update friending.js

--- jsv4/design-patterns/advanced-channel-groups/friending.js
+++ jsv4/design-patterns/advanced-channel-groups/friending.js
@@ -1,48 +1,64 @@
 // ************************************
 // * User A and User B become friends
 // ************************************
- 
+  
 // Add User B to User A's groups: Add ch-user-b-present to cg-user-a-friends
 p.channelGroups.addChannels(
-        {
-            channels: ["ch-user-b-present"],
-                channelGroup: "cg-user-a-friends"  
-        },
-        function (status) {
-                console.log("CG-Add: ", status)
+    {
+        channels: ["ch-user-b-present"],
+        channelGroup: "cg-user-a-friends"  
+    },
+    function(status) {
+        if (status.error) {
+            console.log("operation failed w/ status: ", status);
+        } else {
+            console.log("Channel added to channel group")
+        }
     }
 );
- 
+  
 // Add User B to User A's groups: ch-user-b-status to cg-user-a-status-feed
 p.channelGroups.addChannels(
-        {
-            channels: ["ch-user-b-status"],
-                channelGroup: "cg-user-a-status-feed"
-        },
-        function (status) {
-                console.log("CG-Add: ", status)
+    {
+        channels: ["ch-user-b-status"],
+        channelGroup: "cg-user-a-status-feed"
+    },
+    function(status) {
+        if (status.error) {
+            console.log("operation failed w/ status: ", status);
+        } else {
+            console.log("Channel added to channel group")
+        }
     }
 );
- 
+  
 // Add User A to User B's groups: Add ch-user-a-present to cg-user-b-friends
 p.channelGroups.addChannels(
-        {
-            channels: ["ch-user-a-present"],
-                channelGroup: "cg-user-b-friends"
-        },
-        function (status) {
-                console.log("CG-Add: ", status)
+    {
+        channels: ["ch-user-a-present"],
+        channelGroup: "cg-user-b-friends"
+    },
+    function(status) {
+        if (status.error) {
+            console.log("operation failed w/ status: ", status);
+        } else {
+            console.log("Channel added to channel group")
+        }
     }
 );
- 
+  
 // Add User B to User A's groups: ch-user-a-status to cg-user-b-status-feed
 p.channelGroups.addChannels(
-        {
-                channels: ["ch-user-a-status"],
-                channelGroup: "cg-user-b-status-feed"
-            
+    {
+        channels: ["ch-user-a-status"],
+        channelGroup: "cg-user-b-status-feed"
+         
     },
-        function (status) {
-                console.log("CG-Add: ", status)
+    function(status) {
+        if (status.error) {
+            console.log("operation failed w/ status: ", status);
+        } else {
+            console.log("Channel added to channel group")
+        }
     }
 );
